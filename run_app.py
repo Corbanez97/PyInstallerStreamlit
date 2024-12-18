351048bfbdcc85274ca46ac11bb50c1a8aac64c0
Lucas Corbanez <lucascorbanez@gmail.com>
2024-12-18 16:12:22 -0300

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,5 +1,5 @@
 from streamlit.web import cli
 #this uri depends based on version of your streamlit
 if __name__ == '__main__':
-    cli._main_run_clExplicit('app.py', 'streamlit run')
+    cli._main_run_clExplicit('app.py', is_hello=True, args=['run'])
     #we will CREATE this function inside our streamlit framework
